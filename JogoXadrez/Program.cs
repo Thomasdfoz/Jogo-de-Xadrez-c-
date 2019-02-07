1a48d3c95d02220cd7fc79235e136fb8944d8fb9
Thomas Barros <thomas_sp18@hotmail.com>
2019-02-07 20:48:42 -0200

    Projeto iniciado

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,7 +6,7 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("projeto jogo xadrez");
         }
     }
 }
